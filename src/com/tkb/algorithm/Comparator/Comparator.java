f1703db7a67be5fb4ca20879718bd79214bcaffd
taruncse <tarun.pstu@gmail.com>
2018-02-04 11:26:31 +0600

    Added git ignore

--- src/com/tkb/algorithm/Comparator/Comparator.java
+++ src/com/tkb/algorithm/Comparator/Comparator.java
@@ -1,6 +1,5 @@
 package com.tkb.algorithm.Comparator;
 
-import com.tkb.algorithm.Student;
 
 import java.util.function.Function;
 
@@ -8,19 +7,19 @@ import java.util.function.Function;
 public interface Comparator<T> {
     int compare(T t1, T t2);
 
-    public default Comparator<T> thenComparing (Comparator<T> cmp){
+     default Comparator<T> thenComparing (Comparator<T> cmp){
 
         return (s1,s2) -> compare(s1,s2) == 0 ? cmp.compare(s1,s2): compare(s1,s2);
     }
 
-    public default Comparator<T> thenComparing (Function<T,Comparable> function){
+     default Comparator<T> thenComparing (Function<T,Comparable> f){
 
-        return thenComparing(function);
+        return thenComparing(f);
     }
 
-    public static <U>Comparator<U> comparing(Function<U,Comparable> function){
+     static <U>Comparator<U> comparing(Function<U,Comparable> function){
 
         return (s1,s2)-> function.apply(s1).compareTo(function.apply(s2));
 
-    };
+    }
 }
